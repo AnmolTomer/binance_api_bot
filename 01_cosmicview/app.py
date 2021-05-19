0de1e0bfd69acf2eb3fcfae5481f7451148164a8
Anmol <anmol3540@gmail.com>
2021-05-19 23:07:49 +0530

    Wrote method to buy crypto, view the tokens held and display error if purchase fails.

--- 01_cosmicview/app.py
+++ 01_cosmicview/app.py
@@ -1,7 +1,8 @@
 # Import Flask from flask package
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import config
 import csv
+from binance.enums import *
 
 from binance.client import Client
 
@@ -9,25 +10,38 @@ client = Client(config.API_KEY, config.API_SECRET)
 # Create new app from flask object
 app = Flask(__name__)
 
-
+app.secret_key = 'A0Zr98jyXXHHjmN]LWX'
 # Define routes and the function that will run when that route is called.
+
+
 @app.route('/')
 def index():
     title = 'CosmicView'
     # Display html template using render_template and send it to the page, and we can place the title value
     info = client.get_account()
+
     # print(info)
     """ for inf in info:
         print(inf) """
     balances = info['balances']
-    print(balances)  # Returns list of assets with balance
+    # print(balances)  # Returns list of assets with balance
+    exchange_info = client.get_exchange_info()
+    symbols = exchange_info['symbols']
+    return render_template('index.html', title=title, my_balances=balances, symbols=symbols)
 
-    return render_template('index.html', title=title)
 
-
-@app.route('/buy')
+@app.route('/buy', methods=['POST'])
 def buy():
-    return 'Buy'
+    print(request.form)
+    try:
+        order = client.create_test_order(symbol=request.form['symbol'],
+                                         side=SIDE_BUY,
+                                         type=ORDER_TYPE_MARKET,
+                                         quantity=request.form['quantity'])
+    except Exception as e:
+        flash(e.message, "error")
+
+    return redirect('/')
 
 
 @app.route('/sell')
--- 07_Notes.md
+++ 07_Notes.md
@@ -11,3 +11,15 @@
 ![](https://i.imgur.com/PJP369Z.png)
 
 - In flask we want all of our logic stuff like access to APIs, databases etc. we want on Python. In templates we want to display all of our calculations and information.
+
+- Refer [account endpoints](https://python-binance.readthedocs.io/en/latest/account.html) to see how to place orders.
+
+- Use redirect and flash messages to display the messages when exception thrown while trying to buy coins.
+
+- If order failed then display the error and redirect to index.html.
+
+![](https://i.imgur.com/HtJ6pgZ.png)
+
+- 200 response when order is successful.
+
+![](https://i.imgur.com/nuEBLox.png)
